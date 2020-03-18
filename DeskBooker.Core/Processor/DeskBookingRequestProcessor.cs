6335775a1611d00f6411807df7ad82ba6c6d7d23
Haroon ur Rasheed <haroon@deltashoppe.com>
2020-03-18 17:19:51 +0500

    Work in Progress

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using DeskBooker.Core.Domain;
+using DeskBooker.Core.Interface;
 
 namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessor
     {
-        public DeskBookingRequestProcessor()
+        private readonly IDeskBookingRepository _deskBookingRepository;
+
+        public DeskBookingRequestProcessor(IDeskBookingRepository deskBookingRepository)
         {
+            _deskBookingRepository = deskBookingRepository;
         }
 
         public DeskBookingResult BookDesk(DeskbookingRequest request)
         {
             if (request == null)
                 throw new ArgumentNullException(nameof(request));
+
+            _deskBookingRepository.Save(new DeskBooking()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            });
 
             return new DeskBookingResult()
             {
